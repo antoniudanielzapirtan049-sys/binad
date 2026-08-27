62d37b32738c8fb5f895a7f1c38b357a34c56cc8
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-08-27 22:34:21 +0300

    tarBeforeRm

--- update.sh
+++ update.sh
@@ -8,6 +8,9 @@ test -n "$OWNER"
 test -n "$REPO"
 
 cd $HOME
+TARGETDIR=$HOME/.backups/
+mkdir -p $TARGETDIR/
+tar -C $HOME -czf $TARGETDIR/${REPO}_$(date +%y%m%d_%H%M%S).tar.gz $REPO
 rm -rf $REPO
 git clone https://github.com/$OWNER/$REPO.git
 cd $REPO
